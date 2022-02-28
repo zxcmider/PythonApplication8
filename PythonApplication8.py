3902453467b76d6bb3327c8706d905c0e3fdae32
ghoul sss ahhahahha <93324169+zxcmider@users.noreply.github.com>
2022-02-28 10:41:52 +0200

    Update PythonApplication8.py

--- PythonApplication8.py
+++ PythonApplication8.py
@@ -1,80 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Inimene
+    {
+        string perenimi;
+        public string Status;
+        public int eesnimi;
+        public int shirina;
+        public int vaanus;
+        public int palk;
+        public Inimene() { }
+        
+        public Inimene(string Perenimi)
+        {
+            perenimi = Perenimi;
+        }
+        public Inimene(int Nimi, string Perenimi)
+
+        {
+            eesnimi = Nimi;
+            perenimi = Perenimi;
+        }
+        public int Eesnimi
+        {
+            set { eesnimi = value; }
+            get { return eesnimi; }
+        }
+        public string Perenimi
+        {
+            set { if (perenimi == null) perenimi = value; }
+            get { return perenimi; }
+        }
+        public int Vanus
+        {
+            set
+            {
+                Vanus = value;
+                if (Vanus < 7)
+                {
+                    Status = "Laps";
+                }
+                else if (Vanus < 17)
+                {
+                    Status = "Koolilaps";
+                }
+                else if (Vanus < 24)
+                {
+                    Status = "Ulikooli laps";
+                }
+                else
+                {
+                    Status = "Tööline";
+                }
+            }
+            get { return Vanus; }
+        }
+
+     
+        public void Tervitamine() 
+        {
+        Console.WriteLine("Minu perenimi on" + perenimi);
+        Console.WriteLine("Ma olen {0} aastat vana, olen {1}",vaanus,Status);
+        Console.WriteLine("Minu eesnimi on {0}",eesnimi);
+        }
+    }
+}
 
 
 
-from module1 import*
-from random import *
-import time, sys
-from module1 import *
+________________________________________________
+using System;
 
-users=[]
-passwords=[]
-users=failist_Lugemine("TextFile1.txt",users)
-print(users)
-users=failist_Lugemine('TextFile2.txt',users)
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inimene naine = new Inimene("Kask");
+            naine.Vanus = int.Parse(Console.ReadLine());
+            naine.Tervitamine();
 
-while True:
-	print("Registreerimine = 1 \nAutoriseerimine = 2 \nLogin välja = 3")
-	i=int(input())
-	if i==1:
-		print("Registreemine")
-		while 1:
-			login=input("Kasutajanimi -> ")
-			if login not in users:
-				print("Kasutajanimi soobib")
-				users.append(login)
-				break
-			else:
-				print("See kasutajanimi juba registreeritud!")
-		i=input("Valige, kas soovite parooli luua arvuti(A) või teie enda poolt(I) => ")
-		if i.upper()=="A":
-				pas=autopass()
-				password.append(pas)
-		elif i.upper()=="I":
-			pas=input("Sisestage oma parool -> ")
-			kont=control(password)
-			if kont==True:
-				users.append(login)
-				password.append(pas)
-				break
-			else:
-				print("Parool ei soobib")
-	elif i==2:
-		print("Avtoriseerimine")
-		login=input("Sisestage oma kasutajanimi -> ")
-		passworde=input("Sisestage oma parool -> ")
-		if password.index(passworde)==users.index(login):
-			print("Tere tulemast!")
-			print()
-			while 1:
-				try:
-					play=int(input("Kas soovite mängida arvu 1 kuni 100 ära arvamist? 1 - (jah) 2 - (ei) =>"))
-				except ValueError:
-					print("Ainult numbrid")
-				if play==2:
-					print("Head aega!")
-					exit(0)
-				print ("")
-				print ("Edenemine: 0->1")
-				for i in range(101):
-				   time.sleep(0.1)
-				   update_progress(i/100.0)
-				print ("")
-				print ("Test lõpetatud")
-				time.sleep(0.5)
-				NumberRandom=randint(1,100)
-				User=0
-				while User!=NumberRandom: 
-					User=int(input("Arva ära arv vahemikus 1 kuni 100 => ")) 
-					if User > NumberRandom:
-					  print("Arv peab olema väiksem!") 
-					elif User < NumberRandom: 
-						print("Arv peab olema suurem!") 
-					else: 
-						print("Arvasite ära, see number = ",NumberRandom) 
-				break
-	elif i==3:
-		print("Head aega!")
-		exit(0)
-	else:
-		print("Valige 1,2 või 3!")
-		print("")
+            Inimene mees = new Inimene();
+            mees.Perenimi = "Logopov";
+            mees.Vanus = 45;
+            Console.WriteLine(mees.Perenimi,"on",mees.Vanus,"aastane mees");
+            mees.Tervitamine();
+
+            Inimene[] Inimesed = new Inimene[5]; 
+            for (int i = 0; i < 5; i++)
+            {
+                Inimesed[i] = new Inimene();
+                Console.WriteLine("Mis on sinu nimi");
+                Inimesed[i].Perenimi = Console.ReadLine();
+                Inimesed[i].Vanus = int.Parse(Console.ReadLine());
+            }
+            foreach (Inimene inimene in Inimesed)
+            {
+                inimene.Tervitamine();
+            }   
+
+            Console.ReadKey();
+        }
+    }
+}
